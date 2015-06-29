2627e61e9beae6fd797ac697820025e636d81629
harukaaoki <harukahaoki@gmail.com>
2015-06-29 17:52:21 -0400

    Updates to Week 4 homework

---
+++ Week4/homework/js/main.js
@@ -0,0 +1,62 @@
+/* These are my answers for #1 and #2. 
+
+For link1, I tried changing the colour of the link text (instead of the background colour) to gray by writing 'color grey' but this isn't working - please help! 
+*/
+
+var link1 = document.getElementById('firstLink');
+
+link1.addEventListener('mouseover', function(){
+	link1.setAttribute('style', 'background-color: grey;');
+});
+
+link1.addEventListener('mouseout', function(){
+	link1.setAttribute('style', 'background-color: transparent;');
+});
+
+
+
+
+var link2 = document.getElementById('secondLink');
+
+
+link2.addEventListener("mouseover", function(){
+	link2.setAttribute('style', 'background-color: grey;');
+});
+
+link2.addEventListener("mouseout", function(){
+	link2.setAttribute('style', 'background-color: transparent;');
+});
+
+
+
+var link3 = document.getElementById('thirdLink');
+
+link3.addEventListener("mouseover", function(){
+	link3.setAttribute('style', 'background-color: grey;');
+});
+
+link3.addEventListener("mouseout", function(){
+	link3.setAttribute('style', 'background-color: transparent;');
+});
+
+
+var link4 = document.getElementById('fourthLink');
+
+link4.addEventListener("mouseover", function(){
+	link4.setAttribute('style', 'background-color: grey;');
+});
+
+link4.addEventListener("mouseout", function(){
+	link4.setAttribute('style', 'background-color: transparent;');
+});
+
+
+var link5 = document.getElementById('fifthLink');
+
+link5.addEventListener("mouseover", function(){
+	link5.setAttribute('style', 'background-color: grey;');
+});
+
+link5.addEventListener("mouseout", function(){
+	link5.setAttribute('style', 'background-color: transparent;');
+});
