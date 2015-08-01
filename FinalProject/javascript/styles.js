1052fd6d3472b1a75c5ea15be5c4163a7099e20e
harukaaoki <harukahaoki@gmail.com>
2015-08-01 07:15:56 -0400

    Final Project

--- FinalProject/javascript/styles.js
+++ FinalProject/javascript/styles.js
@@ -8,20 +8,14 @@ $(document).ready(function() {
 	});
 });
 
-// pseudo code for hamburger expansion: 
+/* pseudo code for hamburger expansion: 
 
-// $(document).ready(function() {
-// 	$(when the hamburger button).is clicked on(run this function{
-// 	$(select the hidden menu).toggle this class on();
-// })
-// }); 
+ $(document).ready(function() {
+ 	$(when the hamburger button).is clicked on(run this function{
+ 	$(select the hidden menu).toggle this class on();
+})
+ }); */
 
-
-$("#menu_about").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 4000);
-});
 // scrolling to ABOUT section 
 
 // $(when about in menu).is clicked on(run this function(){
@@ -31,7 +25,11 @@ $("#menu_about").click(function() {
 $("#menu_more_info").click(function() {
     $('html, body').animate({
         scrollTop: $("#more_info").offset().top
-    }, 4000);
+    }, 3000);
 });
 // scrolling to MORE INFO section 
 
+$("#menu_start").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#numberOne").offset().top
+    }, 3000);
